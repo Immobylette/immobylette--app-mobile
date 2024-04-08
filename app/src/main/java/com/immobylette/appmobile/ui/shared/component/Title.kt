72ae6ec1939a8b437f7c297acff1fea019aaad1d
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2024-04-08 12:13:07 +0200

    [UPDATE] added modifier param

--- app/src/main/java/com/immobylette/appmobile/ui/shared/component/Title.kt
+++ app/src/main/java/com/immobylette/appmobile/ui/shared/component/Title.kt
@@ -1,6 +1,7 @@
 package com.immobylette.appmobile.ui.shared.component
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -8,13 +9,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.immobylette.appmobile.ui.shared.theme.ImmobyletteappmobileTheme
 
 @Composable
-fun Title(text: String) {
+fun Title(text: String, modifier: Modifier = Modifier) {
     Text(
         text = text,
+        modifier = modifier,
         fontSize = 20.sp,
         fontWeight = FontWeight.SemiBold,
         color = MaterialTheme.colorScheme.primary,
@@ -27,7 +30,7 @@ fun Title(text: String) {
 fun TitlePreview(){
     ImmobyletteappmobileTheme(darkTheme = false, dynamicColor = false) {
         Surface(Modifier.fillMaxSize()) {
-            Title("Sélectionner un agent")
+            Title("Sélectionner un agent", modifier = Modifier.padding(10.dp))
         }
     }
 }
