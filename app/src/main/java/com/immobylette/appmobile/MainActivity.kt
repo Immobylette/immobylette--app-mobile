9c90df131add3e5d9e9a3601de3bd56f9059ec1f
OdakK <julien.2002@yahoo.fr>
2024-04-15 21:47:09 +0200

    [feat] Add graphic footer component

--- app/src/main/java/com/immobylette/appmobile/MainActivity.kt
+++ app/src/main/java/com/immobylette/appmobile/MainActivity.kt
@@ -12,10 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.immobylette.appmobile.ui.shared.component.GraphicFooter
 import com.immobylette.appmobile.ui.shared.theme.ImmobyletteappmobileTheme
-import java.net.URL
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GraphicFooter()
                 }
             }
         }
