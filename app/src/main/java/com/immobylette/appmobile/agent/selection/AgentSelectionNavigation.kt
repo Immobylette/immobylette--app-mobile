9bfe81cca68520f0dce85ef1f1c518a5c18f7120
Alexis Popieul <99278980+AlexisPop@users.noreply.github.com>
2024-05-02 13:05:50 +0200

    [ADD] Logo component (#7)

--- app/src/main/java/com/immobylette/appmobile/agent/selection/AgentSelectionNavigation.kt
+++ app/src/main/java/com/immobylette/appmobile/agent/selection/AgentSelectionNavigation.kt
@@ -1,9 +1,14 @@
 package com.immobylette.appmobile.agent.selection
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.immobylette.appmobile.ui.shared.component.GraphicFooter
+import com.immobylette.appmobile.ui.shared.component.Logo
 
 const val agentSelectionRoute = "agent-selection"
 
@@ -17,7 +22,14 @@ fun NavGraphBuilder.agentSelectionNavigation(
 
         // Place the page below
         Surface {
-            GraphicFooter()
+            //GraphicFooter()
+            Box(
+                contentAlignment = Alignment.Center, // Centre le contenu à l'intérieur de la Box
+                modifier = Modifier.fillMaxSize() // La Box remplit toute la taille de l'écran
+            ) {
+                Logo();
+            }
+
         }
     }
 }
