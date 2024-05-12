3712e3dbd798f70d466f5f73f8d05c4f710705de
Romain Lefebvre <78411800+patacoing@users.noreply.github.com>
2024-05-12 16:05:59 +0200

    [WIP] store current inventory id

--- app/src/main/java/com/immobylette/appmobile/confirmation/ConfirmationNavigation.kt
+++ app/src/main/java/com/immobylette/appmobile/confirmation/ConfirmationNavigation.kt
@@ -4,19 +4,20 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.immobylette.appmobile.property.current.CurrentPropertyViewModel
+import com.immobylette.appmobile.inventory.current.CurrentInventoryViewModel
 
 const val confirmationRoute = "confirmation"
 
 fun NavGraphBuilder.confirmationNavigation(
-    confirmationViewModel: ConfirmationViewModel,
     currentPropertyViewModel: CurrentPropertyViewModel,
+    currentInventoryViewModel: CurrentInventoryViewModel,
     onNavigateToConfirmed: () -> Unit,
 ) {
     composable(confirmationRoute) {
         ConfirmationPage (
             onNavigateToConfirmed = {
                 val propertyId = currentPropertyViewModel.getId()
-                confirmationViewModel.startInventory(propertyId)
+                currentInventoryViewModel.startInventory(propertyId)
                 onNavigateToConfirmed()
             },
             getTenant = currentPropertyViewModel::getTenant
