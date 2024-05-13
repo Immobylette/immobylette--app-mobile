afb88f243839ca9f117991bbbaa7be7450340386
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2024-05-13 14:06:49 +0200

    [UPDATE] add agentId in request

--- app/src/main/java/com/immobylette/appmobile/data/service/InventoryService.kt
+++ app/src/main/java/com/immobylette/appmobile/data/service/InventoryService.kt
@@ -1,11 +1,13 @@
 package com.immobylette.appmobile.data.service
 
+import com.immobylette.appmobile.data.dto.AgentDto
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Path
 import java.util.UUID
 
 interface InventoryService {
     @POST("properties/{id}/start")
-    suspend fun startInventory(@Path("id") id: UUID): Response<UUID>
+    suspend fun startInventory(@Path("id") id: UUID, @Body agent: AgentDto): Response<UUID>
 }
