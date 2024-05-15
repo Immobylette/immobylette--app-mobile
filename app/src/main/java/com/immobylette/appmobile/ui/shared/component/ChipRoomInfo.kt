b3d10c4a8c07ff49a125b0b0dceb07e4b1edd672
Paul <paullefevre59190@gmail.com>
2024-05-15 23:28:10 +0200

    [UPDATE] Specify height only for Tip modifier

--- app/src/main/java/com/immobylette/appmobile/ui/shared/component/ChipRoomInfo.kt
+++ app/src/main/java/com/immobylette/appmobile/ui/shared/component/ChipRoomInfo.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -48,7 +48,7 @@ fun ChipRoomInfo(
         Spacer(modifier = Modifier.width(5.dp))
         Tip(
             text = "${nbChecked}/${nbTotal}",
-            modifier = Modifier.size(50.dp),
+            modifier = Modifier.height(50.dp),
             style = MaterialTheme.typography.bodyMedium
         )
     }
